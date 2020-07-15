4193c84b06994274cfed038d358b12192e4f3b80
Tamzid Ahmed <tamjidahmed958@gmail.com>
2020-07-15 15:18:17 +0600

    gsap

--- js/app.js
+++ js/app.js
@@ -2,27 +2,29 @@ $(document).ready(function(){
  $("input[name='donation']").change(function(){
     if($(this).val()=="other")
     {
-      $("#otherDonation").show(); 
+      $("#otherDonation").show();
+      TweenMax.from("#otherDonation", 1, {y:-10,scale:0.85});
+      TweenMax.to("#otherDonation", 1, { opacity:1 , delay:2 });
     }
     else
     {
-    $("#otherDonation").hide(); 
+    $("#otherDonation").hide();
     }
-    });
+  });
+
+ 
 
 
 	$('input.bcheck').change(function(){
-		if ($(this).is(':checked')) $('div.div1').show();
+    if ($(this).is(':checked')) 
+    {
+      $('div.div1').show();
+    TweenMax.from(".div1", 1, {y:-10,scale:0.85});
+    TweenMax.to(".div1", 1, { opacity:1 , delay:2 });
+    }
 		else $('div.div1').hide();
     }).change();
     
-    function validateForm() {
-      var x = document.forms["step1"]["fname"].value;
-      if (x == "" || x == null) {
-        alert("Name must be filled out");
-        return false;
-      }
-    }
     
 });
 
